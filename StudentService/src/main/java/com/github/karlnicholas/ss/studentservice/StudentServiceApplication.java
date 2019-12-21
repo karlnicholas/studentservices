552ffa954ca8f48800c9765b304b7999235ca5cc
Karl Nicholas <karl.nicholas@outlook.com>
2019-12-21 04:37:03 -0700

    Remove @EnableWebFlux

--- StudentService/src/main/java/com/github/karlnicholas/ss/studentservice/StudentServiceApplication.java
+++ StudentService/src/main/java/com/github/karlnicholas/ss/studentservice/StudentServiceApplication.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 import com.github.karlnicholas.ss.model.Student;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-@EnableWebFlux
 @RestController
 @RequestMapping("ss")
 public class StudentServiceApplication {
